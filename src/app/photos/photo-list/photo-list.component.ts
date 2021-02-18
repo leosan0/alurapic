30a2b34332822a18b553a351df8d41a6955c0032
Leonardo Okoti <leowokoti@gmail.com>
2021-02-18 16:33:00 -0300

    163218022021

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.services';
 
 @Component({
@@ -7,22 +11,43 @@ import { PhotoService } from '../photo/photo.services';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
-  photos: any[] = [];
+  photos: Photo[] = [];
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
   constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
   ) {}
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
 
-    const userName = this.activatedRoute.snapshot.params.userName;
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load() {
     this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
-
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(
+        photos => {
+          this.photos = this.photos.concat(photos);
+          if(!photos.length) {
+            this.hasMore = false;
+          }
+        }
+      );
   }
 
 }
