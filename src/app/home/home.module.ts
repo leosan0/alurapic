6e6f268456caf54b5a77e9305ee70a7668d9d680
Leonardo Okoti <leowokoti@gmail.com>
2021-02-22 16:53:45 -0300

    165322022021

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SignInComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [ SignInComponent ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class HomeModule {}
