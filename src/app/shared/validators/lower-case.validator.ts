10703a75f6c67fe64fe94759c1042e6fe9ec9390
Leonardo Okoti <leowokoti@gmail.com>
2021-02-24 16:27:27 -0300

    Chapter 4

---
+++ src/app/shared/validators/lower-case.validator.ts
@@ -0,0 +1,10 @@
+import { NullTemplateVisitor } from '@angular/compiler';
+import { AbstractControl } from '@angular/forms';
+
+export function lowerCaseValidator(control: AbstractControl) {
+
+  if(control.value.trim() && !/^[a-z0-9_\-]+$/.test(control.value)) {
+    return { lowerCase: true }
+  }
+  return null;
+}
