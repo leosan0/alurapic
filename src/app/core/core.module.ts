6d8919b4a1b0e9020d1e26dfdaaffc1747cfcedc
Leonardo Okoti <leowokoti@gmail.com>
2021-02-25 16:35:43 -0300

    163525022021

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,30 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { RequestInterceptor } from './auth/request.interceptor';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
-  ]
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
+]
 })
 export class CoreModule {}
--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     PhotoFormComponent
   ],
-  imports: [ CommonModule ]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    VMessageModule,
+    RouterModule
+  ]
 })
-export class PhotoFormModule {
-
-}
+export class PhotoFormModule {}
